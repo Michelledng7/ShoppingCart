bcbdf1a78d3827a4749bc92ff2301ee48713f23f
Michelle <michelledng7@gmail.com>
2023-10-08 01:23:21 +1100

    add ProductProvider.tsx

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, createContext, ReactElement } from 'react'
+
 export type ProductType = {
 	sku: string
 	name: string
@@ -21,3 +23,40 @@ const initState: ProductType[] = [
 		price: 29.99,
 	},
 ]
+
+export type UseProductsContextType = { products: ProductType[] }
+
+const initContextState: UseProductsContextType = { products: [] }
+
+const ProductsContext = createContext<UseProductsContextType>(initContextState)
+
+//create Children type
+type ChildrenType = { children?: ReactElement | ReactElement[] }
+
+export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
+	const [products, setProducts] = useState<ProductType[]>(initState)
+
+	useEffect(() => {
+		const fetchProducts = async (): Promise<ProductType[]> => {
+			const data = await fetch('http://localhost:5500/products')
+				.then(res => {
+					return res.json()
+				})
+				.catch(err => {
+					if (err instanceof Error) console.log(err.message)
+				})
+			return data
+		}
+		fetchProducts().then(products => setProducts(products))
+	}, [])
+
+	return (
+		<ProductsContext.Provider value={{ products }}>
+			{children}
+		</ProductsContext.Provider>
+	)
+}
+
+export default ProductsContext
+
+//npx json-server -w data/products.json -p 5500
