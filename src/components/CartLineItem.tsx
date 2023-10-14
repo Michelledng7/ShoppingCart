7c8e21a757fab828effa591646012efa6b21e151
Michelle <michelledng7@gmail.com>
2023-10-15 01:49:55 +1100

    add memo to skip re-rendering for optimizing

--- src/components/CartLineItem.tsx
+++ src/components/CartLineItem.tsx
@@ -1,7 +1,7 @@
 import { CartItemType } from '../context/CartProvider'
 import { ReducerAction } from '../context/CartProvider'
 import { ReducerActionType } from '../context/CartProvider'
-import { ReactElement, ChangeEvent } from 'react'
+import { ReactElement, ChangeEvent, memo } from 'react'
 
 type PropsType = {
 	item: CartItemType
@@ -88,4 +88,20 @@ const CartLineItem = ({ item, dispatch, REDUCER_ACTIONS }: PropsType) => {
 	return content
 }
 
-export default CartLineItem
+function areItemsEqual(
+	{ item: prevItem }: PropsType,
+	{ item: nextItem }: PropsType
+) {
+	return Object.keys(prevItem).every(key => {
+		return (
+			prevItem[key as keyof CartItemType] ===
+			nextItem[key as keyof CartItemType]
+		)
+	})
+}
+const MemoizedCartLineItem = memo<typeof CartLineItem>(
+	CartLineItem,
+	areItemsEqual
+)
+
+export default MemoizedCartLineItem
