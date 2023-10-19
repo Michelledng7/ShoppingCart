f3e949622ec28b7d2fc7ea8f9c7e1fc6d52a9c82
Michelle <michelledng7@gmail.com>
2023-10-19 16:59:54 +1100

    add function areProductsEqual for memo optimization

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -46,4 +46,20 @@ const Product = ({
 	return content
 }
 
-export default Product
+function areProductsEqual(
+	{ product: prevProduct, inCart: prevInCart }: PropsType,
+	{ product: nextProduct, inCart: nextInCart }: PropsType
+) {
+	return (
+		Object.keys(prevProduct).every(key => {
+			return (
+				prevProduct[key as keyof ProductType] ===
+				nextProduct[key as keyof ProductType]
+			)
+		}) && prevInCart === nextInCart
+	)
+}
+
+const MemoizedProduct = memo(Product, areProductsEqual)
+
+export default MemoizedProduct
--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -7,21 +7,21 @@ export type ProductType = {
 }
 
 const initState: ProductType[] = [
-	{
-		sku: 'item0001',
-		name: 'Widget',
-		price: 9.99,
-	},
-	{
-		sku: 'item0002',
-		name: 'Premium Widget',
-		price: 19.99,
-	},
-	{
-		sku: 'item0003',
-		name: 'Deluxe Widget',
-		price: 29.99,
-	},
+	// {
+	// 	sku: 'item0001',
+	// 	name: 'Widget',
+	// 	price: 9.99,
+	// },
+	// {
+	// 	sku: 'item0002',
+	// 	name: 'Premium Widget',
+	// 	price: 19.99,
+	// },
+	// {
+	// 	sku: 'item0003',
+	// 	name: 'Deluxe Widget',
+	// 	price: 29.99,
+	// },
 ]
 
 export type UseProductsContextType = { products: ProductType[] }
